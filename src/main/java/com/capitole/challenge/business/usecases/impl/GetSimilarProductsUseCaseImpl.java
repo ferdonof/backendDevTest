c11b7ce6500933c09afd0ecf4b2fea521c61b10e
Pablo Fernandez <pmfernan@gmail.com>
2022-12-08 18:07:03 -0300

    Add logging and error handling

--- src/main/java/com/capitole/challenge/business/usecases/impl/GetSimilarProductsUseCaseImpl.java
+++ src/main/java/com/capitole/challenge/business/usecases/impl/GetSimilarProductsUseCaseImpl.java
@@ -2,18 +2,21 @@ package com.capitole.challenge.business.usecases.impl;
 
 import com.capitole.challenge.business.domain.ProductDetailBO;
 import com.capitole.challenge.business.domain.SimilarProductsBO;
+import com.capitole.challenge.business.exceptions.SimilarProductNotFoundException;
 import com.capitole.challenge.business.usecases.GetSimilarProductsUseCase;
 import com.capitole.challenge.ports.ExistingApisPort;
 import org.apache.commons.collections.collection.SynchronizedCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Optional;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class GetSimilarProductsUseCaseImpl implements GetSimilarProductsUseCase {
 
+    private static final Logger logger = LoggerFactory.getLogger(GetSimilarProductsUseCaseImpl.class);
     private final ExistingApisPort port;
 
     public GetSimilarProductsUseCaseImpl(ExistingApisPort port) {
@@ -25,41 +28,55 @@ public class GetSimilarProductsUseCaseImpl implements GetSimilarProductsUseCase 
         AtomicInteger steps = new AtomicInteger();
         SimilarProductsBO similarProductsBO = new SimilarProductsBO();
         SynchronizedCollection.decorate(similarProductsBO.getSimilarProducts());
-        String[] productSimilarIds = port.getProductSimilarIds(productId);
 
         try {
+            logger.info("Request similar products of product id: {}", productId);
+            String[] productSimilarIds = port.getProductSimilarIds(productId);
+            logger.info("Similar ids: {}", productSimilarIds);
+
             ExecutorService executorService = Executors.newFixedThreadPool(10);
             CompletionService<ProductDetailBO> executorCompletionService = new ExecutorCompletionService(executorService);
-            Arrays.stream(productSimilarIds).forEach( id -> executorCompletionService.submit(new Task(id)));
+            Arrays.stream(productSimilarIds).forEach(id -> executorCompletionService.submit(new Task(id)));
 
-            while (steps.get() < productSimilarIds.length ){
+            while (steps.get() < productSimilarIds.length) {
                 steps.incrementAndGet();
                 Optional.ofNullable(executorCompletionService.take().get()).ifPresent(detailBo -> similarProductsBO.getSimilarProducts().add(detailBo));
             }
 
             executorService.shutdownNow();
 
+            logger.error("Returning similar products of product id: {}. [{}] ", productId, similarProductsBO);
+            return similarProductsBO;
+
+        } catch (SimilarProductNotFoundException ex) {
+            logger.error("Exception raised with message: {} ", ex.getMessage());
+            throw ex;
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } catch (ExecutionException e) {
             throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw e;
         }
 
-        return similarProductsBO;
     }
 
     private class Task implements Callable<ProductDetailBO> {
         private final String id;
-        public Task(String id){
+
+        public Task(String id) {
             this.id = id;
         }
 
         @Override
         public ProductDetailBO call() {
             try {
-                return port.getProductById(id);
-            } catch (Exception ex){
-
+                logger.error("Request product with id: {} ", id);
+                ProductDetailBO product = port.getProductById(id);
+                logger.error("Product returned: {} ", product);
+                return product;
+            } catch (Exception ex) {
+                logger.error("Exception raised with message: {} ", ex.getMessage());
             }
             return null;
         }
--- src/main/java/com/capitole/challenge/ports/apis/existingapis/impl/ExistingApisPortImpl.java
+++ src/main/java/com/capitole/challenge/ports/apis/existingapis/impl/ExistingApisPortImpl.java
@@ -1,9 +1,13 @@
 package com.capitole.challenge.ports.apis.existingapis.impl;
 
 import com.capitole.challenge.business.domain.ProductDetailBO;
+import com.capitole.challenge.business.exceptions.ExternalApiException;
+import com.capitole.challenge.business.exceptions.SimilarProductNotFoundException;
 import com.capitole.challenge.dataaccess.apis.ExistingApisConnector;
 import com.capitole.challenge.dataaccess.apis.existingapis.responses.ProductDetail;
 import com.capitole.challenge.ports.ExistingApisPort;
+import feign.FeignException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,7 +21,16 @@ public class ExistingApisPortImpl implements ExistingApisPort {
 
     @Override
     public String[] getProductSimilarIds(String id) {
-        return connector.getProductSimilarIds(id);
+        try {
+            return connector.getProductSimilarIds(id);
+        } catch (FeignException ex) {
+            if (FeignException.FeignClientException.class.isAssignableFrom(ex.getClass()) &&
+                    ex.status() == HttpStatus.NOT_FOUND.value())
+                throw new SimilarProductNotFoundException(id);
+
+            throw new ExternalApiException(id, ex);
+        }
+
     }
 
     @Override
