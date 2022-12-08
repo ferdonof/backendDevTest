1c99a2819045f729513d418d0d1bdc75d0231b18
Pablo Fernandez <pmfernan@gmail.com>
2022-12-08 18:08:21 -0300

    Add exception handlers in the controller

--- src/main/java/com/capitole/challenge/api/controllers/SimilarProductsController.java
+++ src/main/java/com/capitole/challenge/api/controllers/SimilarProductsController.java
@@ -2,7 +2,8 @@ package com.capitole.challenge.api.controllers;
 
 import com.capitole.challenge.api.dtos.responses.SimilarProducts;
 import com.capitole.challenge.api.mappers.SimilarProductsBoToSimilarProductsMapper;
-import com.capitole.challenge.business.domain.SimilarProductsBO;
+import com.capitole.challenge.business.exceptions.ExternalApiException;
+import com.capitole.challenge.business.exceptions.SimilarProductNotFoundException;
 import com.capitole.challenge.business.usecases.GetSimilarProductsUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +23,19 @@ public class SimilarProductsController {
 
     @GetMapping("/{productId}/similar")
     @ResponseStatus(HttpStatus.OK)
-    public SimilarProducts getProductSimilar(@PathVariable(name = "productId") String productId){
+    public SimilarProducts getProductSimilar(@PathVariable(name = "productId") String productId) {
         return mapper.map(useCase.execute(productId));
     }
+
+    @ExceptionHandler(ExternalApiException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String externalApiException(ExternalApiException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(SimilarProductNotFoundException.class)
+    public ResponseEntity<String> resourceNotFoundException(SimilarProductNotFoundException ex) {
+        return ResponseEntity.notFound().build();
+    }
+
 }
